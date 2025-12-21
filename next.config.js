c4142121b334553334755ced5f0de18cdbeccd79
Exequiel Ignacio Sosa <exequielsosa@gmail.com>
2025-12-21 11:18:49 -0300

    Añadir encabezados personalizados para sitemap.xml y robots.txt, y eliminar enlaces alternativos en sitemap.xml para optimizar la configuración SEO.

--- next.config.js
+++ next.config.js
@@ -13,6 +13,28 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/robots.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain; charset=utf-8",
+          },
+        ],
+      },
+      {
         source: "/:all*(svg|jpg|png|webp|avif)",
         headers: [
           {
